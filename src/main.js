69c163b1c742363c2af892ef125e47735c9a374d
liangzhu <1678881243@qq.com>
2020-03-19 10:05:07 +0800

    修复；的bug

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import 'font-awesome/css/font-awesome.min.css'
 
-Vue.config.productionTip = false;
-
-App.mpType = 'app';
-const app = new Vue(App);
-app.$mount();
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue(App)
+app.$mount()
